e7050508342aba89f80ec0a8e90c8e61ab28fe3b
jeremy <jeremy@Sybil>
2018-09-12 18:14:03 +0200

    Add class Departement Add class Employee Update JpaTest.java for the new classes

--- src/main/java/jpa/JpaTest.java
+++ src/main/java/jpa/JpaTest.java
@@ -1,33 +1,64 @@
 package jpa;
 
+import java.util.List;
+
 import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
-import javax.persistence.PersistenceContext;
+import javax.persistence.Persistence;
+
+import enties.Department;
+import enties.Employee;
+
 
 public class JpaTest {
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
+    private EntityManager manager;
 
-		EntityManager manager = EntityManagerHelper.getEntityManager();
-		EntityTransaction tx = manager.getTransaction();
-		tx.begin();
+    public JpaTest(EntityManager manager) {
+        this.manager = manager;
+    }
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        EntityManagerFactory factory =   
+              Persistence.createEntityManagerFactory("example");
+        EntityManager manager = factory.createEntityManager();
+        JpaTest test = new JpaTest(manager);
 
+        EntityTransaction tx = manager.getTransaction();
+        tx.begin();
+        try {
+            test.createEmployees();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        tx.commit();
 
-		try {
+        test.listEmployees();
+            
+        manager.close();
+        System.out.println(".. done");
+    }
 
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		tx.commit();
+    private void createEmployees() {
+        int numOfEmployees = manager.createQuery("Select a From Employee a", Employee.class).getResultList().size();
+        if (numOfEmployees == 0) {
+            Department department = new Department("java");
+            manager.persist(department);
 
+            manager.persist(new Employee("Jakab Gipsz",department));
+            manager.persist(new Employee("Captain Nemo",department));
 
-		manager.close();
-		EntityManagerHelper.closeEntityManagerFactory();
-		//		factory.close();
-	}
+        }
+    }
 
-
+    private void listEmployees() {
+        List<Employee> resultList = manager.createQuery("Select a From Employee a", Employee.class).getResultList();
+        System.out.println("num of employess:" + resultList.size());
+        for (Employee next : resultList) {
+            System.out.println("next employee: " + next);
+        }
+    }
 }
